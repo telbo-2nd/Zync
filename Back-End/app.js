b268e0dd12b759181a53e82dcf2693c2904ccdd3
TELBO <abdelrahmaneltelbany4@gmail.com>
2026-05-17 23:54:51 +0300

    cores2

--- Back-End/app.js
+++ Back-End/app.js
@@ -15,7 +15,16 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://zync-ql3v.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
